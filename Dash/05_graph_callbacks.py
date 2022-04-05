0ff5a2fbb5716b09e6abd87546c3fa19c31737f8
Allan Khariton <allankhariton@gmail.com>
2022-04-05 17:31:56 -0400

    finished graph callback exercise

--- Dash/05_graph_callbacks.py
+++ Dash/05_graph_callbacks.py
@@ -6,7 +6,7 @@ import pandas as pd
 import plotly.graph_objs as go
 
 df = pd.read_csv('../Data/gapminderDataFiveYear.csv')
-
+print(df.head())
 app=dash.Dash()
 
 year_options = []
@@ -15,9 +15,32 @@ for year in df["year"].unique():
 
 app.layout = html.Div([
                     dcc.Graph(id='graph'),
-                    dcc.Dropdown(id='year-picker', options=year_options)
-
+                    dcc.Dropdown(id='year-picker', options=year_options,
+                    value=df['year'].min())
 ])
+
+@app.callback(Output('graph', 'figure'),
+                [Input('year-picker','value')])
+
+def update_figure(selected_year):
+    filtered_df = df[df["year"]==selected_year]
+
+    traces = []
+    for continent_name in filtered_df['continent'].unique():
+        df_by_continent = filtered_df[filtered_df["continent"]==continent_name]
+        traces.append(go.Scatter(
+            x = df_by_continent["gdpPercap"],
+            y = df_by_continent["lifeExp"],
+            mode='markers',
+            opacity=0.7,
+            marker={'size':15},
+            name=continent_name
+        ))
+
+    return {'data':traces,
+            'layout':go.Layout(title='My Plot',
+                                xaxis={'title': 'GDP Per Cap', 'type':'log'},
+                                yaxis={'title':'Life Expectancy'})}
 
 if __name__ == '__main__':
     app.run_server()
