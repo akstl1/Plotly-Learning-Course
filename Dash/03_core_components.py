8e5a5d25bdaaa3f890dbdb498f1d6f0fd314b897
Allan Khariton <allankhariton@gmail.com>
2022-04-04 21:32:33 -0400

    finished core components exercise

--- Dash/03_core_components.py
+++ Dash/03_core_components.py
@@ -12,5 +12,17 @@ app.layout = html.Div([
                                         'value':'NYC'},
                                         {'label':'San Francisco',
                                         'value':'SF'}],
-                            value='SF') #default
+                            value='SF'), #default
+                html.Label('Slider'),
+                dcc.Slider(min=-10, max=10,step=0.5,value=0,marks={i:i for i in range(-10,10)}),
+                html.P(html.Label('Some Radio Items')),
+                dcc.RadioItems(options=[{'label':'New York City',
+                                        'value':'NYC'},
+                                        {'label':'San Francisco',
+                                        'value':'SF'}], value='SF')
 ])
+
+
+# Add the server clause:
+if __name__ == '__main__':
+    app.run_server()
